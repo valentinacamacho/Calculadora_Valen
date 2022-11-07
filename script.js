9b0e72d2e5cbc89db967472aa4a52ab55b5c72dd
valentinacamacho <valentinacamacho0206@gmail.com>
2022-11-06 20:20:01 -0500

    Coloco elementos de iconos para hacer eldesarrollo de modalidad de claro y osucuro en este caso se agrego elementos de html y diseño en css. En otro lado se coloco funcionalidad con JavaScript donde el usuario puede inteeractura con el boton oscuro y claro

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Aqui va la seccion de modo oscuro y claro*/
+const switchButton = document.getElementById('switch');
+switchButton.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    switchButton.classList.toggle('active')
+})
+
+/*Aqui va la seccion de javascript funcion a la calculadora*/
+
