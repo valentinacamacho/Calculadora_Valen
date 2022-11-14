4913c616f2f6128207521c542b0b7bd86de6e2b0
valentinacamacho <valentinacamacho0206@gmail.com>
2022-11-14 17:49:34 -0500

    se coloca los eventos de onclick en cada boton y numero en cuando el usuario le de click le aparezca en pantalla

--- script.js
+++ script.js
@@ -44,3 +44,67 @@ function init(){
 uno.onclick = function(e){
     resultado.textContent = resultado.textContent  + "1";
 }
+/*se llama la variable dos que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 2
+//cuando se hace clic en bloque aparece el numero
+dos.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "2";
+}
+/*se llama la variable tre que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 3
+//cuando se hace clic en bloque aparece el numero
+tres.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "3";
+}
+/*se llama la variable cuatro que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 4
+//cuando se hace clic en bloque aparece el numero
+cuatro.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "4";
+}
+/*se llama la variable cinco que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 5
+//cuando se hace clic en bloque aparece el numero
+cinco.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "5";
+}
+
+/*se llama la variable seis que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 6
+//cuando se hace clic en bloque aparece el numero
+seis.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "6";
+}
+/*se llama la variable siete que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 7
+//cuando se hace clic en bloque aparece el numero
+siete.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "7";
+}
+/*se llama la variable ocho que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 8
+//cuando se hace clic en bloque aparece el numero
+ocho.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "8";
+}
+/*se llama la variable nueve que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 9
+//cuando se hace clic en bloque aparece el numero
+nueve.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "9";
+}
+/*se llama la variable cero que contiene el Dom */
+//se crea una funcion que tenga el argumento e
+//se crea el evento onclick que da cierta funcionalidad cuando se hace clic en el boton numero 0
+//cuando se hace clic en bloque aparece el numero
+cero.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "0";
+}
